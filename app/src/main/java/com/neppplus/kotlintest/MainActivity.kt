be509b2d28abd45a552d1dab26ef68e448eddb34
ujin-Bang <qkddnwls14@gmail.com>
2021-11-02 16:22:46 +0900

    로그인 페이지 세팅부터 ui부터 아이디 부여 if문까지

--- app/src/main/java/com/neppplus/kotlintest/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlintest/MainActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.kotlintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+
+            val inputId = edtID.text.toString()
+            val inputPw = edtPW.text.toString()
+
+            if(inputId == "admin" && inputPw = "qwer") {
+                Toast.makeText(this, "관리자님 환영합니다", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인에 실패했습니다", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
